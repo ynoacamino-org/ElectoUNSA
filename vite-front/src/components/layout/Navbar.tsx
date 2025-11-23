e02210e49aac404e4d70e4ce7e825bc609fc8650
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-11-23 18:36:36 -0500

    Configuracion inicial de arhicvos

--- vite-front/src/components/layout/Navbar.tsx
+++ vite-front/src/components/layout/Navbar.tsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  // Esto luego se reemplaza con Google Auth
+  const [isLoggedIn] = useState(false); 
+
   return (
     <nav className="bg-red-600 text-white py-4 px-8 flex justify-between items-center shadow-md">
+      
       {/* Logo */}
       <div className="flex items-center gap-3">
         <div className="w-8 h-8 bg-white rounded-full"></div>
         <span className="text-lg font-bold tracking-wide">ElectoUNSA</span>
       </div>
 
-      {/* Menú principal */}
+      {/* Menú */}
       <div className="flex gap-6 text-sm font-medium">
         <Link to="/" className="hover:underline decoration-2 underline-offset-4">
           Inicio
@@ -22,20 +27,22 @@ function Navbar() {
         <Link to="/proceso" className="hover:text-gray-200 transition">
           Proceso Electoral
         </Link>
-
-        <Link to="/acceso" className="hover:text-gray-200 transition">
-          Acceso
-        </Link>
-
-        <Link to="/mi-lista" className="hover:text-gray-200 transition">
-          Mi Lista
-        </Link>
       </div>
 
-      {/* Info derecha */}
+      {/* Parte derecha condicional */}
       <div className="flex items-center gap-2">
-        <span className="text-sm">JuntosUNSA</span>
-        <div className="w-8 h-8 bg-white rounded-full"></div>
+        {!isLoggedIn ? (
+          // Estado SIN sesión
+          <Link to="/acceso" className="text-sm hover:text-gray-200 transition">
+            Iniciar sesión
+          </Link>
+        ) : (
+          // Estado CON sesión
+          <Link to="/mi-lista" className="flex items-center gap-2 hover:text-gray-200 transition">
+            <span className="text-sm">JuntosUNSA</span>
+            <div className="w-8 h-8 bg-white rounded-full"></div>
+          </Link>
+        )}
       </div>
     </nav>
   );
--- vite-front/src/pages/Home/HomePage.tsx
+++ vite-front/src/pages/Home/HomePage.tsx
@@ -1,6 +1,6 @@
 function HomePage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen ">
       <h1 className="text-4xl font-bold text-unsa-granate">Hola Mundo</h1>
     </div>
   );
